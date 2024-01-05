3737c70adeb38888e21306859ce45630952b8b1b
Jonathan <jonathan@forestandrock.co.uk>
2024-01-06 00:42:37 +0100

    added some comments + black formatting

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,15 +1,27 @@
-import os
+"""
+Production Flask Example
+
+"""
+
 from flask import Flask, request
 from celery import Celery, Task, shared_task
 
 
 def create_celery_app(app):
+    """
+    Create Celery App
+
+    Creates a celery app object using config details from the Flask app
+    sent as the first positional argument.
+    """
+
     class FlaskTask(Task):
         def __call__(self, *args, **kwargs):
             with app.app_context():
                 return self.run(*args, **kwargs)
+
     celery_app = Celery(app.name, task_cls=FlaskTask)
-    celery_app.config_from_object(app.config['CELERY'])
+    celery_app.config_from_object(app.config["CELERY"])
     celery_app.set_default()
     return celery_app
 
@@ -21,21 +33,25 @@ def create_app(test_config=None):
         CELERY={
             "broker_url": "redis://localhost",
             "result_backend": "redis://localhost",
-            "task_ignore_result": True
+            "task_ignore_result": True,
         }
     )
 
+    # This is a route that does not depend on celery
+    # If this works, then the flask app is 'working'.
     @app.route("/hello")
     def hello():
         return "hello world"
 
-    app.extensions['celery'] = create_celery_app(app)
+    app.extensions["celery"] = create_celery_app(app)
 
     @shared_task(ignore_result=False)
     def add_together(a, b):
         return a + b
 
-
+    # Here we have the route with the celery task.
+    # At the moment it just returns the id of the result, not the
+    # actual value.
     @app.route("/add")
     def add():
         a = request.args.get("a")
@@ -43,10 +59,4 @@ def create_app(test_config=None):
         result = add_together.delay(float(a), float(b))
         return "result:" + str(result)
 
-
-
     return app
-
-if __name__ == "__main__":
-    flask_app = create_app()
-    celery_app = flask_app.extensions["celery"]
